b7903374a6673ec229050514a58ba9a157423c4c
Afkre <ahmetefkere@gmail.com>
2023-06-08 23:53:00 +0200

    2306082330

---
+++ StringMethoden/Main.java
@@ -0,0 +1,67 @@
+package StringMethoden;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        
+        //System.out.println(b);
+        
+        /*System.out.println("BuschtanenZahl: " + b.length());
+        
+        System.out.println("0.index : "+ b.charAt(0));
+        System.out.println("2.index : "+ b.charAt(2));
+        System.out.println("Letzte Element : "+ b.charAt(b.length()-1));*/
+        
+        /*for (int i = 0; i < b.length();i++) {
+            
+            System.out.println(b.charAt(i));
+            
+        }*/
+        /*System.out.println(b.startsWith("Mu"));
+        System.out.println(b.endsWith("CO"));*/
+        
+        //System.out.println(b.indexOf('a'));
+        //System.out.println(b.lastIndexOf('a'));
+        
+        /*System.out.println(b.toLowerCase());
+        System.out.println(b.toUpperCase());*/
+        
+        /*String a1 = "1971";
+        
+        int b1 = Integer.parseInt(a1);
+        
+        System.out.println(b1 - 42);*/
+        
+       /* int b1 = 1971;
+        String a1 = String.valueOf(b1);
+        
+        System.out.println(a1);*/
+       
+        
+        /*String a1 = "Ahmet";
+        String a2 = "Peter";
+        
+        if (a1 == a2 ) {
+            System.out.println("Gleich");
+        }*/
+        String b1 = new String("Ahmet");
+        String b2 = new String("Ahmet");
+        
+        if (b1.equals(b2)) {
+            System.out.println("Gleich");
+        }
+       // String b = new String("Ahmet");
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
+

c079845eb6b81a79fe051e7bfdc0f800e36734d2
Afkre <ahmetefkere@gmail.com>
2023-06-09 23:55:57 +0200

    2306092330

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,5 +1,26 @@
 package LinkedList;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> programmierSprachen = new ArrayList<String>();
+
+        programmierSprachen.add("Python");
+        programmierSprachen.add("Java");
+        programmierSprachen.add("Php");
+        programmierSprachen.add("JavaScript");
+        programmierSprachen.add("C#");
+        
+    for(int i=0; i<programmierSprachen.size(); i++){
+        System.out.println(programmierSprachen.get(i));
+    }
+    
+    }
+    
+    
     
 }
+
